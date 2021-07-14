a90e9ee03e97daecdc2f2171809dab5715dd6939
theWellHopeErr <ssuryarajan@gmail.com>
2021-07-14 20:03:02 +0530

    Add BottomNavigationBar in HomeWidget

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portal_phase_ii_ui/Notification/notification.dart';
+import 'package:portal_phase_ii_ui/work-order/WorkOrder.dart';
 
 class HomeWidget extends StatefulWidget {
   final String plant;
@@ -17,9 +19,48 @@ class _HomeWidgetState extends State<HomeWidget> {
 
   _HomeWidgetState(this.plant, this.plantGroup);
 
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    WorkOrderWidget(),
+    NotificationWidget(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    print('Plant: $plant \nPlant Group: $plantGroup');
-    return Container(child: Text('Home'));
+    return Scaffold(
+      body: Center(
+        child: DefaultTextStyle.merge(
+          style: TextStyle(color: Colors.grey[850]),
+          child: Container(
+            color: Colors.white,
+            width: double.infinity,
+            height: double.infinity,
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long),
+            label: 'Work Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.campaign),
+            label: 'Notifications',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
