a4851b8b43251df6ca184ffb8a656fa4af6a858b
theWellHopeErr <ssuryarajan@gmail.com>
2021-08-03 19:26:06 +0530

    Integrate Push Notifications with FCM

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:overlay_support/overlay_support.dart';
+
 import 'package:portal_phase_ii_ui/notification/notifications-list.dart';
 import 'package:portal_phase_ii_ui/work-order/workorders-list.dart';
+
+class PushNotification {
+  PushNotification({
+    this.title,
+    this.body,
+    this.dataTitle,
+    this.dataBody,
+  });
+
+  String? title;
+  String? body;
+  String? dataTitle;
+  String? dataBody;
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+}
 
 class HomeWidget extends StatefulWidget {
   final String plant;
@@ -14,6 +36,11 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  late final FirebaseMessaging _messaging;
+  late int _notificationNotifCount;
+  late int _workorderNotifCount;
+  PushNotification? _notificationInfo;
+
   final String plant;
   final String planGroup;
 
@@ -29,6 +56,115 @@ class _HomeWidgetState extends State<HomeWidget> {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  void registerNotification() async {
+    await Firebase.initializeApp();
+    _messaging = FirebaseMessaging.instance;
+    print("FCM Token: ${await _messaging.getToken()}");
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print(
+            'Message title: ${message.notification?.title}, body: ${message.notification?.body}, data: ${message.data}');
+
+        // Parse the message received
+        PushNotification notification = PushNotification(
+          title: message.notification?.title,
+          body: message.notification?.body,
+          dataTitle: message.data['title'],
+          dataBody: message.data['body'],
+        );
+
+        setState(() {
+          _notificationInfo = notification;
+          if (notification.title!.contains('Notification'))
+            _notificationNotifCount++;
+          if (notification.title!.contains('Work Order'))
+            _workorderNotifCount++;
+        });
+
+        if (_notificationInfo != null) {
+          // For displaying the notification as an overlay
+          showSimpleNotification(
+            Text(_notificationInfo!.title!),
+            leading: NotificationBadge(
+                totalNotifications: notification.title!.contains('Work Order')
+                    ? _workorderNotifCount
+                    : _notificationNotifCount),
+            slideDismissDirection: DismissDirection.horizontal,
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.blueAccent,
+            foreground: Colors.white,
+            duration: Duration(seconds: 5),
+          );
+        }
+      });
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
+  // For handling notification when the app is in terminated state
+  checkForInitialMessage() async {
+    await Firebase.initializeApp();
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      PushNotification notification = PushNotification(
+        title: initialMessage.notification?.title,
+        body: initialMessage.notification?.body,
+        dataTitle: initialMessage.data['title'],
+        dataBody: initialMessage.data['body'],
+      );
+
+      setState(() {
+        _notificationInfo = notification;
+        if (notification.title!.contains('Notification'))
+          _notificationNotifCount++;
+        if (notification.title!.contains('Work Order')) _workorderNotifCount++;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _notificationNotifCount = 0;
+
+    _workorderNotifCount = 0;
+    registerNotification();
+    checkForInitialMessage();
+
+    // For handling notification when the app is in background
+    // but not terminated
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      PushNotification notification = PushNotification(
+        title: message.notification?.title,
+        body: message.notification?.body,
+        dataTitle: message.data['title'],
+        dataBody: message.data['body'],
+      );
+
+      setState(() {
+        _notificationInfo = notification;
+        if (notification.title!.contains('Notification'))
+          _notificationNotifCount++;
+        if (notification.title!.contains('Work Order')) _workorderNotifCount++;
+      });
+    });
+
+    super.initState();
   }
 
   @override
@@ -49,17 +185,108 @@ class _HomeWidgetState extends State<HomeWidget> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.receipt_long),
+            icon: Stack(
+              children: <Widget>[
+                Icon(
+                  Icons.receipt_long,
+                  size: 30,
+                ),
+                Visibility(
+                  visible: _workorderNotifCount != 0,
+                  child: Positioned(
+                    right: 0,
+                    child: Container(
+                      padding: EdgeInsets.all(1),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      constraints: BoxConstraints(
+                        minWidth: 15,
+                        minHeight: 15,
+                      ),
+                      child: Text(
+                        '$_workorderNotifCount',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 9,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
             label: 'Work Orders',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.campaign),
+            icon: Stack(
+              children: <Widget>[
+                Icon(
+                  Icons.campaign,
+                  size: 30,
+                ),
+                Visibility(
+                  visible: _notificationNotifCount != 0,
+                  child: Positioned(
+                    right: 0,
+                    child: Container(
+                      padding: EdgeInsets.all(1),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      constraints: BoxConstraints(
+                        minWidth: 15,
+                        minHeight: 15,
+                      ),
+                      child: Text(
+                        '$_notificationNotifCount',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 9,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
             label: 'Notifications',
           ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class NotificationBadge extends StatelessWidget {
+  final int totalNotifications;
+
+  const NotificationBadge({required this.totalNotifications});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40.0,
+      height: 40.0,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            '$totalNotifications',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:portal_phase_ii_ui/helpers.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 import 'package:portal_phase_ii_ui/home.dart';
 import 'package:portal_phase_ii_ui/login.dart';
+import 'package:portal_phase_ii_ui/helpers.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,21 +17,23 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return MaterialApp(
-      title: 'Maintenance Portal',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF591EFF),
-        primaryColorLight: Color(0xFF9977F9),
-        primaryColorDark: Color(0xFF7B1FA2),
-        accentColor: Color(0xFF9C27B0),
-        scaffoldBackgroundColor: Color(0xFF7C4DFF),
-        textTheme: ThemeData.dark().textTheme.apply(
-              fontFamily: 'Montserrat',
-            ),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: LandingWidget(),
+    return OverlaySupport(
+      child: MaterialApp(
+        title: 'Maintenance Portal',
+        theme: ThemeData.dark().copyWith(
+          primaryColor: Color(0xFF591EFF),
+          primaryColorLight: Color(0xFF9977F9),
+          primaryColorDark: Color(0xFF7B1FA2),
+          accentColor: Color(0xFF9C27B0),
+          scaffoldBackgroundColor: Color(0xFF7C4DFF),
+          textTheme: ThemeData.dark().textTheme.apply(
+                fontFamily: 'Montserrat',
+              ),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: LandingWidget(),
+        ),
       ),
     );
   }
